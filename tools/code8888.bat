970268c8039c026b9956f38856230eab43be845d
wjc <wanjochan@gmail.com>
2025-01-03 11:30:54 +0800

    ；

--- tools/code8888.bat
+++
@@ -1,1 +0,0 @@
-code --proxy-server=http://127.0.0.1:8888 --host-resolver-rules="MAP * 0.0.0.0, EXCLUDE 127.0.0.1,localhost"
--- tools/cursor8888.bat
+++
@@ -1,1 +0,0 @@
-cursor --proxy-server=http://127.0.0.1:8888 --host-resolver-rules="MAP * 0.0.0.0, EXCLUDE 127.0.0.1,localhost"
