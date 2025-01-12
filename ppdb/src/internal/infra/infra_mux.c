90e4040e8b4fa3171ebad866e58991a7053a5217
wjc <wanjochan@gmail.com>
2025-01-12 23:57:58 +0800

    cancel iocp (cosmo good iocp already)

--- ppdb/src/internal/infra/infra_mux.c
+++ ppdb/src/internal/infra/infra_mux.c
@@ -4,14 +4,9 @@
 
 #include "infra_mux.h"
 #include "infra_mux_epoll.h"
-#include "infra_mux_iocp.h"
 #include "infra_platform.h"
 
 infra_error_t infra_mux_create(const infra_config_t* config, infra_mux_t** mux) {
     if (!config || !mux) { return INFRA_ERROR_INVALID_PARAM; }
-    if (g_infra.platform.is_windows) {
-	    //TODO && !edge_trigger
-        return infra_mux_iocp_create(config, mux);
-    }
     return infra_mux_epoll_create(config, mux);
 } 
