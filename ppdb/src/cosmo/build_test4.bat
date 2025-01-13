a0b0ab0a0b91591defcc96a9a8251f37bc600c87
wjc <wanjochan@gmail.com>
2025-01-13 18:28:45 +0800

    try

--- ppdb/src/cosmo/build_test4.bat
+++ ppdb/src/cosmo/build_test4.bat
@@ -6,26 +6,30 @@ set CROSS9=..\..\..\repos\cross9
 set COSMO=..\..\..\repos\cosmopolitan
 set PATH=%CROSS9%\bin;%PATH%
 
-REM 验证头文件和库文件存在
+REM 验证文件存在
 if not exist "%COSMO%\cosmopolitan.h" (
     echo Error: cosmopolitan.h not found in %COSMO%
+    exit /b 1
+)
+if not exist "%COSMO%\cosmopolitan.a" (
+    echo Error: cosmopolitan.a not found in %COSMO%
     exit /b 1
 )
 
 REM 设置编译器和选项
 set CC=x86_64-pc-linux-gnu-gcc
-set CFLAGS=-g -fPIC -fvisibility=hidden -nostdinc -nostdlib -fno-pie -fno-stack-protector -mno-red-zone -O2 -I%COSMO%
-set LDFLAGS=-nostdlib -nostartfiles -static -T dll.lds -Wl,--version-script=exports.txt
+set CFLAGS=-g -fPIC -fvisibility=hidden -nostdinc -nostdlib -fno-pie -fno-stack-protector -mno-red-zone -O2 -I%COSMO% -mcmodel=medium
+set LDFLAGS=-nostdlib -nostartfiles -static -T dll.lds -Wl,--version-script=exports.txt -z max-page-size=0x1000
 
 REM 编译动态库
 echo Building test4.dl...
 %CC% %CFLAGS% -c test4.c -o test4.o
-%CC% %LDFLAGS% test4.o -o test4.dl
+%CC% %LDFLAGS% -Wl,--image-base=0x220000000 test4.o %COSMO%\cosmopolitan.a %COSMO%\ape.o -o test4.dl
 
 REM 编译测试程序
 echo Building test4_main...
 %CC% %CFLAGS% -c test4_main.c -o test4_main.o
-%CC% -nostdlib -nostartfiles -static test4_main.o %COSMO%\ape.o -o test4_main.exe
+%CC% -nostdlib -nostartfiles -static -Wl,-z,max-page-size=0x1000 -Wl,--build-id=none test4_main.o %COSMO%\ape.o %COSMO%\cosmopolitan.a -o test4_main.exe
 
 echo Build complete.
 endlocal 
