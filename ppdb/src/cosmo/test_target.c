ebd22647507508375c4a13258a262b8e9a238974
wjc <wanjochan@gmail.com>
2025-01-18 13:53:28 +0800

    no message

--- ppdb/src/cosmo/test_target.c
+++ ppdb/src/cosmo/test_target.c
@@ -1,16 +1,14 @@
 #include "cosmopolitan.h"
 
-int test_func(int a, int b) {
-    return a + b;
-}
-
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     printf("Hello from test_target!\n");
-    printf("1 + 2 = %d\n", test_func(1, 2));
+    printf("Arguments: %d\n", argc);
+    for(int i = 0; i < argc; i++) {
+        printf("  argv[%d] = %s\n", i, argv[i]);
+    }
     return 0;
 }
 
-void _start(void) {
-    static char* argv[] = {"test_target.com", NULL};
-    exit(main(1, argv));
-} 
+//void _start(void) {
+//    exit(main(__argc, __argv));
+//} 
