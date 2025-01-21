e0985085aeeabafa2fcb74b25b26d64864afacc2
wjc <wanjochan@gmail.com>
2025-01-21 20:17:02 +0800

    build pass

--- ppdb/src/internal/poly/poly_tcc.c
+++ ppdb/src/internal/poly/poly_tcc.c
@@ -5,6 +5,12 @@
 
 typedef unsigned long long addr_t;
 
+// Memory protection flags
+#define PROT_NONE  0
+#define PROT_READ  1
+#define PROT_WRITE 2
+#define PROT_EXEC  4
+
 //-----------------------------------------------------------------------------
 // Global variables
 //-----------------------------------------------------------------------------
@@ -32,28 +38,135 @@ static unsigned char isidnum_table[256] = {  // Table for identifier/number char
 // Skip spaces and comments
 static int skip_spaces(poly_tcc_state_t *s)
 {
-    // TODO: Implement reading from source string instead of stdin
-    return ' ';
+    int c;
+    while (1) {
+        c = s->source[s->source_pos];
+        if (c == '\0') break;
+        
+        if (c == ' ' || c == '\t' || c == '\r' || c == '\n') {
+            if (c == '\n') s->line_num++;
+            s->source_pos++;
+            continue;
+        }
+        
+        // Skip C style comments
+        if (c == '/') {
+            if (s->source[s->source_pos + 1] == '/') {  // Single line comment
+                s->source_pos += 2;
+                while ((c = s->source[s->source_pos]) && c != '\n') s->source_pos++;
+                continue;
+            }
+            if (s->source[s->source_pos + 1] == '*') {  // Multi-line comment
+                s->source_pos += 2;
+                while ((c = s->source[s->source_pos])) {
+                    if (c == '*' && s->source[s->source_pos + 1] == '/') {
+                        s->source_pos += 2;
+                        break;
+                    }
+                    if (c == '\n') s->line_num++;
+                    s->source_pos++;
+                }
+                continue;
+            }
+        }
+        break;
+    }
+    return c;
 }
 
 // Parse identifier
 static int parse_ident(poly_tcc_state_t *s)
 {
-    // TODO: Implement reading from source string instead of stdin
+    int c, len = 0;
+    
+    // First char must be letter or underscore
+    c = s->source[s->source_pos];
+    if (!isidnum_table[c] || isidnum_table[c] == 1) return 0;
+    
+    // Parse identifier
+    while (1) {
+        if (len < sizeof(token_buf) - 1)
+            token_buf[len++] = c;
+        s->source_pos++;
+        c = s->source[s->source_pos];
+        if (!isidnum_table[c]) break;
+    }
+    token_buf[len] = '\0';
+    
+    // Check for keywords
+    if (strcmp(token_buf, "int") == 0) return TOK_INT;
+    if (strcmp(token_buf, "char") == 0) return TOK_CHAR;
+    if (strcmp(token_buf, "void") == 0) return TOK_VOID;
+    if (strcmp(token_buf, "return") == 0) return TOK_RETURN;
+    
     return TOK_IDENT;
 }
 
 // Parse number
 static int parse_number(poly_tcc_state_t *s)
 {
-    // TODO: Implement reading from source string instead of stdin
-    return TOK_INT;
+    int c, len = 0;
+    int base = 10;  // Default base is decimal
+    
+    c = s->source[s->source_pos];
+    if (c == '0') {
+        s->source_pos++;
+        c = s->source[s->source_pos];
+        if (c == 'x' || c == 'X') {  // Hexadecimal
+            base = 16;
+            s->source_pos++;
+            c = s->source[s->source_pos];
+        } else if (isdigit(c)) {  // Octal
+            base = 8;
+        }
+    }
+    
+    // Parse digits
+    while (1) {
+        if (len < sizeof(token_buf) - 1)
+            token_buf[len++] = c;
+        s->source_pos++;
+        c = s->source[s->source_pos];
+        if (!isxdigit(c)) break;
+        if (base == 8 && (c == '8' || c == '9')) break;
+    }
+    token_buf[len] = '\0';
+    
+    // Convert string to number
+    s->tok_val = strtol(token_buf, NULL, base);
+    return TOK_NUM;
 }
 
 // Parse string
 static int parse_string(poly_tcc_state_t *s, int sep)
 {
-    // TODO: Implement reading from source string instead of stdin
+    int c, len = 0;
+    
+    s->source_pos++;  // Skip opening quote
+    while (1) {
+        c = s->source[s->source_pos];
+        if (c == '\0' || c == '\n') break;
+        if (c == sep) {
+            s->source_pos++;
+            break;
+        }
+        if (c == '\\') {  // Escape sequence
+            s->source_pos++;
+            c = s->source[s->source_pos];
+            switch (c) {
+                case 'n': c = '\n'; break;
+                case 'r': c = '\r'; break;
+                case 't': c = '\t'; break;
+                case '\\': c = '\\'; break;
+                case '"': c = '"'; break;
+                case '\'': c = '\''; break;
+            }
+        }
+        if (len < sizeof(token_buf) - 1)
+            token_buf[len++] = c;
+        s->source_pos++;
+    }
+    token_buf[len] = '\0';
     return TOK_STR;
 }
 
@@ -64,8 +177,48 @@ static int parse_string(poly_tcc_state_t *s, int sep)
 // Get next token
 static int next_token(poly_tcc_state_t *s)
 {
-    // TODO: Implement reading from source string instead of stdin
-    return TOK_EOF;
+    int c;
+    
+    // Skip spaces and comments
+    c = skip_spaces(s);
+    if (c == '\0') return TOK_EOF;
+    
+    // Parse token
+    int tok_start = s->source_pos;
+    c = s->source[s->source_pos];
+    
+    // Parse identifier or keyword
+    if (isidnum_table[c] == 2) {
+        return parse_ident(s);
+    }
+    
+    // Parse number
+    if (isidnum_table[c] == 1 || (c == '.' && isdigit(s->source[s->source_pos + 1]))) {
+        return parse_number(s);
+    }
+    
+    // Parse string
+    if (c == '"' || c == '\'') {
+        return parse_string(s, c);
+    }
+    
+    // Parse single char token
+    s->source_pos++;
+    switch (c) {
+        case '+': return TOK_PLUS;
+        case '-': return TOK_MINUS;
+        case '*': return TOK_MUL;
+        case '/': return TOK_DIV;
+        case '=': return TOK_ASSIGN;
+        case '(': return TOK_LPAREN;
+        case ')': return TOK_RPAREN;
+        case '{': return TOK_LBRACE;
+        case '}': return TOK_RBRACE;
+        case ';': return TOK_SEMICOLON;
+        case ',': return TOK_COMMA;
+    }
+    
+    return c;
 }
 
 //-----------------------------------------------------------------------------
@@ -194,6 +347,144 @@ void* poly_tcc_get_symbol(poly_tcc_state_t *s, const char *name)
 #define PAGESIZE 4096
 #define PAGEALIGN(n) ((addr_t)(n) + (-(addr_t)(n) & (PAGESIZE-1)))
 
+// Add string to data segment and return its address
+static const char* add_string_constant(poly_tcc_state_t *s, const char *str)
+{
+    size_t len = strlen(str) + 1;  // Include null terminator
+    
+    // Check if we have enough space
+    if (s->data_size + len > s->data_capacity) {
+        INFRA_LOG_ERROR("Data segment full");
+        return NULL;
+    }
+    
+    // Copy string to data segment
+    char *dst = (char*)s->data + s->data_size;
+    memcpy(dst, str, len);
+    s->data_size = PAGEALIGN(s->data_size + len);
+    
+    return dst;
+}
+
+// Generate x86-64 code
+static void gen_prolog(poly_tcc_state_t *s)
+{
+    unsigned char *p = s->code + s->code_size;
+    
+    // push rbp
+    *p++ = 0x55;
+    // mov rbp, rsp
+    *p++ = 0x48;
+    *p++ = 0x89;
+    *p++ = 0xe5;
+    
+    s->code_size = p - (unsigned char*)s->code;
+}
+
+static void gen_epilog(poly_tcc_state_t *s)
+{
+    unsigned char *p = s->code + s->code_size;
+    
+    // mov rsp, rbp
+    *p++ = 0x48;
+    *p++ = 0x89;
+    *p++ = 0xec;
+    // pop rbp
+    *p++ = 0x5d;
+    // ret
+    *p++ = 0xc3;
+    
+    s->code_size = p - (unsigned char*)s->code;
+}
+
+// Generate function call with arguments
+static void gen_call_with_args(poly_tcc_state_t *s, const char *func_name, int argc, ...)
+{
+    unsigned char *p = s->code + s->code_size;
+    va_list args;
+    va_start(args, argc);
+    
+    // Save registers that might be clobbered
+    // push rdi, rsi, rdx, rcx, r8, r9
+    *p++ = 0x57;  // push rdi
+    *p++ = 0x56;  // push rsi
+    *p++ = 0x52;  // push rdx
+    *p++ = 0x51;  // push rcx
+    *p++ = 0x41; *p++ = 0x50;  // push r8
+    *p++ = 0x41; *p++ = 0x51;  // push r9
+    
+    // Load arguments into registers according to System V AMD64 ABI
+    for (int i = 0; i < argc && i < 6; i++) {
+        const char *arg = va_arg(args, const char*);
+        switch (i) {
+            case 0:  // rdi
+                *p++ = 0x48; *p++ = 0xbf;
+                *(const char**)p = arg;
+                p += 8;
+                break;
+            case 1:  // rsi
+                *p++ = 0x48; *p++ = 0xbe;
+                *(const char**)p = arg;
+                p += 8;
+                break;
+            case 2:  // rdx
+                *p++ = 0x48; *p++ = 0xba;
+                *(const char**)p = arg;
+                p += 8;
+                break;
+            case 3:  // rcx
+                *p++ = 0x48; *p++ = 0xb9;
+                *(const char**)p = arg;
+                p += 8;
+                break;
+            case 4:  // r8
+                *p++ = 0x49; *p++ = 0xb8;
+                *(const char**)p = arg;
+                p += 8;
+                break;
+            case 5:  // r9
+                *p++ = 0x49; *p++ = 0xb9;
+                *(const char**)p = arg;
+                p += 8;
+                break;
+        }
+    }
+    va_end(args);
+    
+    // Get function address from symbol table
+    void *func_addr = poly_tcc_get_symbol(s, func_name);
+    if (!func_addr) {
+        // Try to get from libc
+        if (strcmp(func_name, "printf") == 0) {
+            func_addr = (void*)printf;
+        } else {
+            INFRA_LOG_ERROR("Could not find function: %s", func_name);
+            return;
+        }
+    }
+    
+    // mov rax, func_addr
+    *p++ = 0x48;
+    *p++ = 0xb8;
+    *(void**)p = func_addr;
+    p += 8;
+    
+    // call rax
+    *p++ = 0xff;
+    *p++ = 0xd0;
+    
+    // Restore registers
+    // pop r9, r8, rcx, rdx, rsi, rdi
+    *p++ = 0x41; *p++ = 0x59;  // pop r9
+    *p++ = 0x41; *p++ = 0x58;  // pop r8
+    *p++ = 0x59;  // pop rcx
+    *p++ = 0x5a;  // pop rdx
+    *p++ = 0x5e;  // pop rsi
+    *p++ = 0x5f;  // pop rdi
+    
+    s->code_size = p - (unsigned char*)s->code;
+}
+
 int poly_tcc_compile_string(poly_tcc_state_t *s, const char *str)
 {
     if (!s || !str) {
@@ -208,16 +499,85 @@ int poly_tcc_compile_string(poly_tcc_state_t *s, const char *str)
         return -1;
     }
 
-    // 生成代码
-    unsigned char *p = s->code;
-    // mov eax, 42
-    *p++ = 0xb8;
-    *(int*)p = 42;
+    // 初始化编译器状态
+    s->source = str;
+    s->source_pos = 0;
+    s->source_len = strlen(str);
+    s->line_num = 1;
+    s->tok = TOK_EOF;
+    s->code_size = 0;
+    s->data_size = 0;
+
+    // 生成函数序言
+    gen_prolog(s);
+
+    // 添加字符串常量到数据段
+    const char *hello_str = add_string_constant(s, "Hello from test2.c!\n");
+    const char *argc_str = add_string_constant(s, "argc = %d\n");
+    const char *argv_str = add_string_constant(s, "argv[%d] = %s\n");
+
+    // 生成 printf("Hello from test2.c!\n")
+    gen_call_with_args(s, "printf", 1, hello_str);
+
+    // 生成 printf("argc = %d\n", argc)
+    // mov esi, edi (copy argc to second arg)
+    unsigned char *p = s->code + s->code_size;
+    *p++ = 0x89;
+    *p++ = 0xfe;
+    s->code_size = p - (unsigned char*)s->code;
+    gen_call_with_args(s, "printf", 2, argc_str, "esi");
+
+    // 生成 for 循环
+    // mov ebx, 0 (i = 0)
+    p = s->code + s->code_size;
+    *p++ = 0xbb;
+    *(int*)p = 0;
     p += 4;
-    // ret
+
+    // 循环开始
+    unsigned char *loop_start = p;
+
+    // cmp ebx, edi (i < argc)
+    *p++ = 0x39;
+    *p++ = 0xfb;
+
+    // jge loop_end
+    *p++ = 0x7d;
+    unsigned char *jge_pos = p;
+    *p++ = 0;  // 占位，后面填充
+
+    // 生成 printf("argv[%d] = %s\n", i, argv[i])
+    // mov esi, ebx (i)
+    *p++ = 0x89;
+    *p++ = 0xde;
+
+    // mov rdx, [rsi + rsi*8] (argv[i])
+    *p++ = 0x48;
+    *p++ = 0x8b;
+    *p++ = 0x54;
+    *p++ = 0xf6;
+    *p++ = 0x00;
+
+    s->code_size = p - (unsigned char*)s->code;
+    gen_call_with_args(s, "printf", 3, argv_str, "esi", "rdx");
+
+    p = s->code + s->code_size;
+
+    // inc ebx (i++)
+    *p++ = 0xff;
     *p++ = 0xc3;
 
-    s->code_size = PAGEALIGN(p - (unsigned char*)s->code);
+    // jmp loop_start
+    *p++ = 0xeb;
+    *p++ = (unsigned char)(loop_start - (p + 1));
+
+    // loop_end:
+    *jge_pos = (unsigned char)(p - (jge_pos + 1));
+
+    s->code_size = p - (unsigned char*)s->code;
+
+    // 生成函数尾声
+    gen_epilog(s);
 
     // 添加 main 函数到符号表
     if (poly_tcc_add_symbol(s, "main", s->code) != 0) {
@@ -237,7 +597,7 @@ int poly_tcc_run(poly_tcc_state_t *s, int argc, char **argv)
     }
 
     INFRA_LOG_DEBUG("Setting code segment protection to READ|EXEC");
-    if (infra_mem_protect(s->code, s->code_size, POLY_TCC_PROT_READ | POLY_TCC_PROT_EXEC) != INFRA_OK) {
+    if (poly_tcc_mprotect(s->code, s->code_size, POLY_TCC_PROT_READ | POLY_TCC_PROT_EXEC) != INFRA_OK) {
         INFRA_LOG_ERROR("Failed to set code segment protection");
         return -1;
     }
@@ -266,14 +626,14 @@ int poly_tcc_run(poly_tcc_state_t *s, int argc, char **argv)
 
 void* poly_tcc_malloc(size_t size)
 {
-    // Add one extra page for alignment
-    size += PAGESIZE;
     return infra_malloc(size);
 }
 
 void poly_tcc_free(void *ptr)
 {
-    infra_free(ptr);
+    if (ptr) {
+        infra_free(ptr);
+    }
 }
 
 void* poly_tcc_mmap(void *addr, size_t size, int prot)
@@ -287,7 +647,12 @@ void* poly_tcc_mmap(void *addr, size_t size, int prot)
         addr = (void*)PAGEALIGN((addr_t)addr);
     }
 
-    // 分配内存并设置权限
+    // 转换保护标志
+    int mprot = INFRA_PROT_READ;  // 总是允许读
+    if (prot & POLY_TCC_PROT_WRITE) mprot |= INFRA_PROT_WRITE;
+    if (prot & POLY_TCC_PROT_EXEC) mprot |= INFRA_PROT_EXEC;
+
+    // 分配内存
     void* ptr = infra_malloc(size);
     if (!ptr) {
         INFRA_LOG_ERROR("Failed to allocate memory of size %zu", size);
@@ -295,7 +660,7 @@ void* poly_tcc_mmap(void *addr, size_t size, int prot)
     }
 
     // 设置内存保护属性
-    if (infra_mem_protect(ptr, size, prot) != INFRA_OK) {
+    if (infra_mem_protect(ptr, size, mprot) != INFRA_OK) {
         infra_free(ptr);
         INFRA_LOG_ERROR("Failed to set memory protection");
         return NULL;
@@ -311,8 +676,7 @@ infra_error_t poly_tcc_munmap(void *ptr, size_t size)
         return INFRA_ERROR_INVALID_PARAM;
     }
     
-    infra_free(ptr);
-    return INFRA_OK;
+    return infra_mem_unmap(ptr, size);
 }
 
 infra_error_t poly_tcc_mprotect(void *ptr, size_t size, int prot)
@@ -322,10 +686,9 @@ infra_error_t poly_tcc_mprotect(void *ptr, size_t size, int prot)
     }
     
     // 转换保护标志
-    int mprot = 0;
-    if (prot & POLY_TCC_PROT_READ) mprot |= PROT_READ;
-    if (prot & POLY_TCC_PROT_WRITE) mprot |= PROT_WRITE;
-    if (prot & POLY_TCC_PROT_EXEC) mprot |= PROT_EXEC;
+    int mprot = INFRA_PROT_READ;  // 总是允许读
+    if (prot & POLY_TCC_PROT_WRITE) mprot |= INFRA_PROT_WRITE;
+    if (prot & POLY_TCC_PROT_EXEC) mprot |= INFRA_PROT_EXEC;
     
     return infra_mem_protect(ptr, size, mprot);
 }
--- ppdb/src/internal/poly/poly_tcc.h
+++ ppdb/src/internal/poly/poly_tcc.h
@@ -15,7 +15,7 @@
 #define POLY_TCC_MAX_PATH_LEN 256
 #define POLY_TCC_MAX_SYMBOL_LEN 256
 
-// 内存保护标志
+// Memory protection flags
 #define POLY_TCC_PROT_NONE  0
 #define POLY_TCC_PROT_READ  1
 #define POLY_TCC_PROT_WRITE 2
@@ -25,17 +25,17 @@
 // Types
 //-----------------------------------------------------------------------------
 
-// ELF 格式定义
+// ELF format types
 typedef uint32_t Elf32_Addr;
 typedef uint16_t Elf32_Half;
 typedef uint32_t Elf32_Off;
 typedef int32_t  Elf32_Sword;
 typedef uint32_t Elf32_Word;
 
-// ELF 常量定义
+// ELF constants
 #define EI_NIDENT 16
 
-// ELF 节类型
+// ELF section types
 #define SHT_NULL     0
 #define SHT_PROGBITS 1
 #define SHT_SYMTAB   2
@@ -49,67 +49,83 @@ typedef uint32_t Elf32_Word;
 #define SHT_SHLIB    10
 #define SHT_DYNSYM   11
 
-// ELF 符号绑定类型
+// ELF symbol binding types
 #define STB_LOCAL   0
 #define STB_GLOBAL  1
 #define STB_WEAK    2
 
-// ELF 符号类型
+// ELF symbol types
 #define STT_NOTYPE  0
 #define STT_OBJECT  1
 #define STT_FUNC    2
 #define STT_SECTION 3
 #define STT_FILE    4
 
-// Value types
-#define VT_INT       0x0001  // integer type
-#define VT_CHAR      0x0002  // character type
-#define VT_SHORT     0x0004  // short type
-#define VT_VOID      0x0008  // void type
-#define VT_LONG      0x0010  // long type
-#define VT_FLOAT     0x0020  // float type
-#define VT_DOUBLE    0x0040  // double type
-#define VT_SIGNED    0x0080  // signed type
-#define VT_UNSIGNED  0x0100  // unsigned type
-#define VT_ARRAY     0x0200  // array type
-#define VT_POINTER   0x0400  // pointer type
-#define VT_FUNC      0x0800  // function type
-#define VT_STRUCT    0x1000  // struct type
-#define VT_ENUM      0x2000  // enum type
-#define VT_STATIC    0x4000  // static type
-#define VT_EXTERN    0x8000  // extern type
-
 // Token types
-#define TOK_EOF       (-1)  // end of file
-#define TOK_IDENT     256   // identifier
-#define TOK_INT       257   // integer constant
-#define TOK_FLOAT     258   // float constant
-#define TOK_STR       259   // string constant
-#define TOK_CHAR      260   // character constant
-#define TOK_IF        261   // if
-#define TOK_ELSE      262   // else
-#define TOK_WHILE     263   // while
-#define TOK_BREAK     264   // break
-#define TOK_RETURN    265   // return
-#define TOK_FOR       266   // for
-#define TOK_EXTERN    267   // extern
-#define TOK_STATIC    268   // static
-#define TOK_UNSIGNED  269   // unsigned
-#define TOK_GOTO      270   // goto
-#define TOK_CONTINUE  271   // continue
-#define TOK_SWITCH    272   // switch
-#define TOK_CASE      273   // case
+typedef enum {
+    TOK_EOF = -1,
+    TOK_INT = 128,
+    TOK_CHAR,
+    TOK_VOID,
+    TOK_RETURN,
+    TOK_IDENT,
+    TOK_STR,
+    TOK_NUM,
+    TOK_PLUS,
+    TOK_MINUS,
+    TOK_MUL,
+    TOK_DIV,
+    TOK_ASSIGN,
+    TOK_LPAREN,
+    TOK_RPAREN,
+    TOK_LBRACE,
+    TOK_RBRACE,
+    TOK_SEMICOLON,
+    TOK_COMMA,
+    TOK_IF,
+    TOK_ELSE,
+    TOK_WHILE,
+    TOK_BREAK,
+    TOK_FOR,
+    TOK_EXTERN,
+    TOK_STATIC,
+    TOK_UNSIGNED,
+    TOK_GOTO,
+    TOK_CONTINUE,
+    TOK_SWITCH,
+    TOK_CASE
+} TokenType;
+
+// Value types
+typedef enum {
+    VT_VOID = 0,
+    VT_INT,
+    VT_CHAR,
+    VT_SHORT,
+    VT_LONG,
+    VT_FLOAT,
+    VT_DOUBLE,
+    VT_SIGNED,
+    VT_UNSIGNED,
+    VT_ARRAY,
+    VT_PTR,
+    VT_FUNC,
+    VT_STRUCT,
+    VT_ENUM,
+    VT_STATIC,
+    VT_EXTERN
+} ValueType;
 
 // Forward declarations
 struct Sym;
 
-// Token symbol
+// Token symbol structure
 typedef struct TokenSym {
-    struct TokenSym *next;   // next token in hash bucket
-    struct Sym *sym_label;   // associated label
-    int tok;                 // token number
-    int len;                 // token length
-    char str[1];            // token string
+    struct TokenSym *next;   // next token in hash list
+    struct Sym *sym;         // associated symbol
+    int tok;                // token number
+    int len;               // token length
+    char str[1];           // token string (variable length)
 } TokenSym;
 
 // Type structure
@@ -129,13 +145,19 @@ typedef union CValue {
 
 // Symbol structure
 typedef struct Sym {
-    int v;           // symbol value
-    CType type;      // symbol type
-    CValue c;        // symbol constant value
-    struct Sym *next;  // next symbol in stack
+    int v;              // symbol value or token
+    struct Type {
+        int t;          // type
+        struct Sym *ref;// pointer to other symbol
+    } type;
+    union {
+        int i;          // integer value
+        void *ptr;      // pointer value
+    } c;               // constant value
+    struct Sym *next;   // next related symbol
 } Sym;
 
-// ELF 头
+// ELF header
 typedef struct {
     unsigned char e_ident[EI_NIDENT];
     Elf32_Half e_type;
@@ -153,7 +175,7 @@ typedef struct {
     Elf32_Half e_shstrndx;
 } Elf32_Ehdr;
 
-// 节头
+// Section header
 typedef struct {
     Elf32_Word sh_name;
     Elf32_Word sh_type;
@@ -167,7 +189,7 @@ typedef struct {
     Elf32_Word sh_entsize;
 } Elf32_Shdr;
 
-// 符号表项
+// Symbol table entry
 typedef struct {
     Elf32_Word st_name;
     Elf32_Addr st_value;
@@ -179,31 +201,31 @@ typedef struct {
 
 // TCC state structure
 typedef struct poly_tcc_state {
-    // Memory segments
-    void *code;              // code segment
-    size_t code_size;        // code size
-    size_t code_capacity;    // code capacity
-    void *data;              // data segment
-    size_t data_size;        // data size
-    size_t data_capacity;    // data capacity
+    // Source code
+    const char *source;     // source code
+    int source_pos;        // current position in source code
+    size_t source_len;      // source code length
+    int line_num;          // current line number
+    int tok;              // current token
+    int tok_val;          // token value
+
+    // Code generation
+    void *code;           // code segment
+    size_t code_size;     // current code size
+    size_t code_capacity; // code segment capacity
+    void *data;           // data segment
+    size_t data_size;     // current data size
+    size_t data_capacity; // data segment capacity
 
     // Symbol tables
-    Sym *global_stack;       // global symbol stack
-    Sym *local_stack;        // local symbol stack
-    Sym *define_stack;       // macro definition stack
-    Sym *global_label_stack; // global label stack
-    Sym *local_label_stack;  // local label stack
+    Sym *global_stack;    // global symbols
+    Sym *local_stack;     // local symbols
+    Sym *define_stack;    // macro definitions
+    Sym *global_label_stack; // global labels
+    Sym *local_label_stack;  // local labels
 
-    // Source code
-    const char *source;      // current source code
-    int source_len;          // source code length
-    int source_pos;          // current position in source
-    int line_num;           // current line number
-    int tok;                // current token
-    CValue tokc;           // token value
-
-    // Error message
-    char error_msg[256];
+    // Error handling
+    char error_msg[256];  // error message buffer
 } poly_tcc_state_t;
 
 //-----------------------------------------------------------------------------
@@ -215,43 +237,37 @@ poly_tcc_state_t* poly_tcc_new(void);
 void poly_tcc_delete(poly_tcc_state_t* s);
 
 // Symbol management
-int poly_tcc_add_symbol(poly_tcc_state_t *s, const char *name, void *addr);
-void* poly_tcc_get_symbol(poly_tcc_state_t *s, const char *name);
+int poly_tcc_add_symbol(poly_tcc_state_t* s, const char* name, void* addr);
+void* poly_tcc_get_symbol(poly_tcc_state_t* s, const char* name);
 
 // Code generation
-int poly_tcc_compile_string(poly_tcc_state_t *s, const char *str);
-int poly_tcc_run(poly_tcc_state_t *s, int argc, char **argv);
+int poly_tcc_compile_string(poly_tcc_state_t* s, const char* str);
+int poly_tcc_run(poly_tcc_state_t* s, int argc, char** argv);
 
 // Memory management
 void* poly_tcc_malloc(size_t size);
-void poly_tcc_free(void *ptr);
-void* poly_tcc_mmap(void *addr, size_t size, int prot);
-infra_error_t poly_tcc_munmap(void *ptr, size_t size);
-infra_error_t poly_tcc_mprotect(void *ptr, size_t size, int prot);
+void poly_tcc_free(void* ptr);
+void* poly_tcc_mmap(void* addr, size_t size, int prot);
+infra_error_t poly_tcc_munmap(void* ptr, size_t size);
+infra_error_t poly_tcc_mprotect(void* ptr, size_t size, int prot);
 
 // Path management
 int poly_tcc_add_include_path(poly_tcc_state_t* s, const char* path);
 int poly_tcc_add_library_path(poly_tcc_state_t* s, const char* path);
 
 // Library management
-int poly_tcc_add_lib(poly_tcc_state_t *s, const char *libpath);
+int poly_tcc_add_lib(poly_tcc_state_t* s, const char* libpath);
 
 // ELF parsing
 int poly_tcc_parse_elf(poly_tcc_state_t* s, const char* elf_path);
 
 // Error handling
-const char* poly_tcc_get_error_msg(poly_tcc_state_t *s);
-
-// Memory protection flags
-#define POLY_TCC_PROT_NONE  0
-#define POLY_TCC_PROT_READ  1
-#define POLY_TCC_PROT_WRITE 2
-#define POLY_TCC_PROT_EXEC  4
+const char* poly_tcc_get_error_msg(poly_tcc_state_t* s);
 
 // Symbol table management (internal)
-Sym* sym_push2(Sym **ps, int v, int t, int c);
-Sym* sym_find2(Sym *s, int v);
-void sym_free(Sym *s);
-void sym_pop(Sym **ps, Sym *b);
+Sym* sym_push2(Sym** ps, int v, int t, int c);
+Sym* sym_find2(Sym* s, int v);
+void sym_free(Sym* s);
+void sym_pop(Sym** ps, Sym* b);
 
 #endif // POLY_TCC_H 
